d2d5d0570912cd9edcee0be8bded37dc91f6c148
NIL2000 <nilabhra09b.net@gmail.com>
2024-05-26 10:21:15 +0530

    Course entry page for teacher done

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,36 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function Coursespage() {
+
+export default async function Coursespage() {
+
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect('/');
+  }
+  const courses = await db.course.findMany({
+    where: {
+      userId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
+      {/* <Link href="/teacher/create">
         <Button>New course</Button>
-      </Link>
+      </Link> */}
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 }
